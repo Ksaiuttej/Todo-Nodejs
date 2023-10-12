dc4df5935627d68cfcec1f9921e42b0be7291403
Ksaiuttej <125295613+Ksaiuttej@users.noreply.github.com>
2023-10-12 12:56:23 +0530

    Update index.js removed sessions

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
-import session from 'express-session';
-import { randomBytes } from 'crypto';
-
 
 const app = express();
 const port = process.env.PORT || 3000;
 const arr_item = [];
 const arr_items = [];
-const secretKey = randomBytes(64).toString('hex');
 
 
 
 app.use(express.static("public"));
-app.use(session({
-    secret: secretKey,
-    resave: false,
-    saveUninitialized: true
-  }));
-
-app.use(session({
-    secret: secretKey,
-    resave: false,
-    saveUninitialized: true
-  }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -32,27 +17,21 @@ app.get("/", (req,res) => {
 })
 
 app.get("/today", (req,res) => {
-    res.render("today.ejs",{finalArray :req.session.today_items})
+    res.render("today.ejs",{finalArray :arr_item})
 })
 
 app.post("/submitToday", (req,res) => {
-    if (!req.session.today_items) {
-        req.session.today_items = [];
-      }
-      req.session.today_items.push(req.body.today_item)
-    res.render("today.ejs",{finalArray : req.session.today_items})
+    arr_item.push(req.body.today_item)
+    res.render("today.ejs",{finalArray : arr_item})
 })
 
 app.get("/month", (req,res) => {
-    res.render("month.ejs",{finalArray : req.session.month_items})
+    res.render("month.ejs",{finalArray : arr_items})
 })
 
 app.post("/submitMonth", (req,res) => {
-    if (!req.session.month_items) {
-        req.session.month_items = [];
-      }
-      req.session.month_items.push(req.body.month_item)
-    res.render("month.ejs",{finalArray : req.session.month_items})
+      arr_items.push(req.body.month_item)
+    res.render("month.ejs",{finalArray : arr_items})
 })
 
 
