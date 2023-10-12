66ffbe166cd270a09ee0ff25a564496a9078081d
Ksaiuttej <ksaiuttej@gmail.com>
2023-10-12 12:46:31 +0530

    Your commit message second

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import session from 'express-session';
+import { randomBytes } from 'crypto';
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const arr_item = [];
 const arr_items = [];
-
+const secretKey = randomBytes(64).toString('hex');
 
 
 
 app.use(express.static("public"));
+app.use(session({
+    secret: secretKey,
+    resave: false,
+    saveUninitialized: true
+  }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,21 +26,27 @@ app.get("/", (req,res) => {
 })
 
 app.get("/today", (req,res) => {
-    res.render("today.ejs",{finalArray : arr_item})
+    res.render("today.ejs",{finalArray :req.session.today_items})
 })
 
 app.post("/submitToday", (req,res) => {
-    arr_item.push(req.body.today_item)
-    res.render("today.ejs",{finalArray : arr_item})
+    if (!req.session.today_items) {
+        req.session.today_items = [];
+      }
+      req.session.today_items.push(req.body.today_item)
+    res.render("today.ejs",{finalArray : req.session.today_items})
 })
 
 app.get("/month", (req,res) => {
-    res.render("month.ejs",{finalArray : arr_items})
+    res.render("month.ejs",{finalArray : req.session.month_items})
 })
 
 app.post("/submitMonth", (req,res) => {
-    arr_items.push(req.body.month_item)
-    res.render("month.ejs",{finalArray : arr_items})
+    if (!req.session.month_items) {
+        req.session.month_items = [];
+      }
+      req.session.month_items.push(req.body.month_item)
+    res.render("month.ejs",{finalArray : req.session.month_items})
 })
 
 
